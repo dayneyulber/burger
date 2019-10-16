145c074a13119dea6c3ae94b432cbb7e4d94aa08
dayne beronque <dayneyulber@gmail.com>
2019-10-16 14:23:33 -0500

    initial db changes

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Monterrey Melt');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi');
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Sylvester', true);
