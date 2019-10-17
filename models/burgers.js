abb11ff3e6b450dfb85a3ec4a0cd1dbbc527c45b
dayne beronque <dayneyulber@gmail.com>
2019-10-17 17:35:44 -0500

    fixed burgers display on command line

---
+++ models/burgers.js
@@ -0,0 +1,26 @@
+var orm = require("./../config/orm.js");
+var burger = {
+
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+  insertOne: function (vals, cb) {
+    orm.insertOne("burgers", "name", vals, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: function (objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+  deleteOne: function (condition, cb) {
+    orm.deleteOne("burgers", condition, function (res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
