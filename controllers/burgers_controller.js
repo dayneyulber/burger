6484dbcb4eceb125ca965330ae410c536dfeed30
dayne beronque <dayneyulber@gmail.com>
2019-10-17 17:32:45 -0500

    required objects placed + router setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var burger = require("./../models/burgers.js");
+var router = express.Router();
+
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne(req.body.burger, function (result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burger.updateOne({
+    devoured: true
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  burger.deleteOne(condition, function (result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
